e967f1c3e74969be559c18b293416269d3cb2575
bosco <boscotang98@gmail.com>
2025-01-22 21:22:58 +0800

    git config fix on includedUsers

--- modules/git/hm-module.nix
+++ modules/git/hm-module.nix
@@ -7,47 +7,44 @@
 }: 
 
 let
-  includedUsers = lib.filter (lib.strings.hasSuffix ".nix") (lib.filesystem.listFilesRecursive ./extra-users);
+  includedUsers = lib.filter (lib.strings.hasSuffix ".nix") (lib.filesystem.listFilesRecursive ./extras);
 in
 {
-  # manage by hm, please only be imported in hm-module
-  config = lib.mkIf config.programs.git.enable {
-    programs.git = {
-      lfs.enable = true;
+  programs.git = {
+    lfs.enable = true;
 
-      userName = vars.defaultGitUsername;
-      userEmail = vars.defaultGitMail;
+    userName = vars.defaultGitUsername;
+    userEmail = vars.defaultGitMail;
 
-      includes = includedUsers;
+    includes = [] ++ includedUsers;
 
-      ignores = [
-        ".DS_Store"
-      ];
+    ignores = [
+      ".DS_Store"
+    ];
 
-      aliases = {
-        prettylog = "log --pretty=format:'%C(Yellow)%h%x09%Creset%ai%x09%Cgreen%an %Cblue(%ae)%Creset: %s'";
-        undo = "reset --soft HEAD^";
-        cancel = "reset --hard HEAD^";
-        onemore = "commit -a --amend --no-edit";
+    aliases = {
+      prettylog = "log --pretty=format:'%C(Yellow)%h%x09%Creset%ai%x09%Cgreen%an %Cblue(%ae)%Creset: %s'";
+      undo = "reset --soft HEAD^";
+      cancel = "reset --hard HEAD^";
+      onemore = "commit -a --amend --no-edit";
+    };
+
+    # pager
+    delta = {
+      enable = true;
+      options = {
+        side-by-side = true;
+        navigate = true;
       };
+    };
 
-      # pager
-      delta = {
-        enable = true;
-        options = {
-          side-by-side = true;
-          navigate = true;
-        };
-      };
+    extraConfig = {
+      push.autoSetupRemote = true;
+      pull.rebase = false;
 
-      extraConfig = {
-        push.autoSetupRemote = true;
-        pull.rebase = false;
-
-        core = {
-          sshCommand = "ssh -i ~/.ssh/id_ed25519_default";
-          editor = "vim";
-        };
+      core = {
+        sshCommand = "ssh -i ~/.ssh/id_ed25519_default";
+        editor = "vim";
       };
     };
   };
